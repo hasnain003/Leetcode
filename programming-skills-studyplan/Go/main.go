669d0f1ff61b18c2b8c607794c015c1dc0164b56
hasnain003 <mdhasnain839@gmail.com>
2023-08-08 00:52:03 +0530

    8th august question done

--- programming-skills-studyplan/Go/main.go
+++ programming-skills-studyplan/Go/main.go
@@ -1,14 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	binarySum := addBinary("11", "10")
-	fmt.Println(fmt.Sprintf("binary sum calculated over string 11 and 10 is : %v", binarySum))
+	fmt.Printf("binary sum calculated over string 11 and 10 is : %v", binarySum)
 
 	fmt.Printf("Pow calculated recursively: %v\n", RecursivePow(2.0, 12))
 	fmt.Printf("Pow calculated iteratively: %v\n", IterativePow(2.0, -12))
 
-	fmt.Printf("Result of multiplicate of two string 123456789979 and 87765655429763: %v", multiply("123456789979", "87765655429763"))
+	fmt.Printf("Result of multiplicate of two string 123456789979 and 87765655429763: %v\n", multiply("123456789979", "87765655429763"))
+	list1 := &ListNode{Val: 1}
+	list1.Next = &ListNode{Val: 2}
+	list1.Next.Next = &ListNode{Val: 5}
+	list2 := &ListNode{Val: 1}
+	list2.Next = &ListNode{Val: 3}
+	list2.Next.Next = &ListNode{Val: 4}
+	list := mergeTwoLists(list1, list2)
+	log.Printf("Merge List from list1 %v and list2 %v is: %v", list1, list2, list)
+
+	fmt.Printf("reverse of List: %v", reverseList(list))
 
 }
---
+++ programming-skills-studyplan/Go/merge_list.go
@@ -0,0 +1,37 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil && list2 == nil {
+		return nil
+	}
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	current := &ListNode{}
+	newHead := current
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else if list2.Val < list1.Val {
+			current.Next = list2
+			list2 = list2.Next
+		}
+		current = current.Next
+	}
+	if list1 == nil {
+		current.Next = list2
+	} else {
+		current.Next = list1
+	}
+	return newHead.Next
+}
